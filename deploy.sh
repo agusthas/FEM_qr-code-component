eff1a3c56d7d4f9d6a2d44addf94ceac9ed7b7cf
SusuMantan <suzumantan@gmail.com>
2022-02-18 01:52:55 +0000

    fix: update git repo and branch

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 
-git push -f git@github.com:agusthas/FEM_time-tracking-dashboard-main.git master:gh-pages
+git push -f git@github.com:agusthas/FEM_qr-code-component.git main:gh-pages
 
 cd -
